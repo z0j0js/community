455901596ff0c020ff4bb5a5c8c717e253d1a56e
z0j0js <783155876@qq.com>
2020-05-21 15:32:19 +0800

    keep github login status

--- src/main/java/com/sise/community/controller/AuthorizeController.java
+++ src/main/java/com/sise/community/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package com.sise.community.controller;
 
 import com.sise.community.dto.AccessTokenDTO;
 import com.sise.community.dto.GithubUser;
+import com.sise.community.mapper.UserMapper;
+import com.sise.community.model.User;
 import com.sise.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,7 +11,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -26,10 +31,14 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
-                           HttpServletRequest request){
+                           HttpServletRequest request,
+                           HttpServletResponse response){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -37,12 +46,19 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        if(user != null){
-            //登录成功，写cookie和session
-            request.getSession().setAttribute("user",user);
-            return "redirect:/";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if(githubUser != null){
+            User user = new User();
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.getGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            response.addCookie(new Cookie("token",token));
 
+            return "redirect:/";
         }else {
             //登录失败，重新登录
             return "redirect:/";
---
+++ src/main/java/com/sise/community/mapper/UserMapper.java
@@ -0,0 +1,22 @@
+package com.sise.community.mapper;
+
+import com.sise.community.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+    @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+
+    /**
+     * 在mybaties中，方法参数是类的话，会自动将参数类中成员变量填入到sql语句中 #{} 对应位置
+     * 但如果方法参数不是类，是变量的话，需要添加 @Param 注解，注解的value的值填写需要填入到sql语句中 #{} 对应位置的值
+     * @param token
+     * @return
+     */
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
+}
